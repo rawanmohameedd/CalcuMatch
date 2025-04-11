946f9752b271e3b18dbf72348f4e8a48d90026e1
rawanmohameedd <nrawana12345@gmail.com>
2025-04-11 19:11:23 +0200

    protectedLayouy

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import TablePage from './pages/TablePage';
-import "./App.css"
+import "./App.css";
+import { ReactNode } from 'react';
+
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const token = localStorage.getItem('token');
+  
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
+  
+  return children;
+};
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/table" element={<TablePage />} />
+        <Route 
+          path="/table" 
+          element={
+            <ProtectedRoute>
+              <TablePage />
+            </ProtectedRoute>
+          } 
+        />
+        {/* Catch-all route to redirect to login */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
